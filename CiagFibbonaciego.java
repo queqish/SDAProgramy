150a9aee392a3c027e886dd311ffe3862c0e451f
queqish <adamowicz.p.zseil@gmail.com>
2018-04-24 18:03:35 +0200

    Dodanie programu

---
+++ CiagFibbonaciego.java
@@ -0,0 +1,17 @@
+package pl.sda.pierwszezajecia;
+
+public class CiagFibbonaciego {
+
+    public static void main(String[] args) {
+
+//        int n=10;
+//
+//        for(int i; i<n;i++){
+//
+//            for(int j; j<n; j++){
+//
+//
+//            }
+//        }
+    }
+}
